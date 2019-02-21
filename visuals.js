d76afcc3e31abe8d4de079d72947f284c8a97f4b
Eldritch Cheese <eldritch.cheese@gmail.com>
2019-02-20 18:17:59 -0800

    Painted a census with the room's visual.

--- visuals.js
+++ visuals.js
@@ -1,28 +1,36 @@
 var spawning = require('spawning');
 
+function census_by_list(creep_list) {
+    var count = {};
+    creep_list.forEach(creep => {
+        count[creep.memory.role] = (count[creep.memory.role] || 0) + 1;
+    });
+
+    return Object.entries(count)
+        .key_sort(e => e[0])
+        .map(e => e[0] + ': ' + e[1])
+        .join('\n')
+    ;
+}
+
 function census(room) {
     var census = {
-        exist: {},
-        queued: {},
+        exist: Object.values(Game.creeps),
+        queued: spawning.queue,
     };
-    Object.values(Game.creeps)
-        .forEach(creep => {
-            if(census.exist[creep.memory.role] === undefined) {
-                census.exist[creep.memory.role] = 0;
-            }
-            census.exist[creep.memory.role]++;
-        });
 
-    spawning.queue
-        .forEach(creep => {
-            if(census.queued[creep.memory.role] === undefined) {
-                census.queued[creep.memory.role] = 0;
-            }
-            census.queued[creep.memory.role]++;
-        });
+    var census_text = Object.entries(census)
+        .map(e => e[0] + '\n' + census_by_list(e[1]))
+        .join('\n');
 
-
-    return census;
+    census_text
+        .split(/\n/)
+        .forEach( (text,line_num) => {
+            room.visual.text(text,
+                             5, 5+line_num,
+                             {align: 'left'});
+        })
+    ;
 }
 
 
